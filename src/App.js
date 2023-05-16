fd00c5d3c50c2838c0ee2bcf9bbb16409e46a29b
Abdur Rahman Rakib <arrakib.ux@gmail.com>
2023-05-16 22:15:12 +0600

    updated-feature

--- src/App.js
+++ src/App.js
@@ -1,32 +1,24 @@
-import React from 'react'
-// import Article from './components/article/Article'
-// import Brand from './components/brand/Brand'
-// import CTA from './components/cta/CTA'
-// import Feature from './components/feature/Feature'
-// import Navbar from './components/navbar/Navbar'
+import React from 'react';
 
-// we can write all of this line in a single line because of index.js file
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './container';
+import { CTA, Brand, Navbar } from './components';
 
-import { Blog,Header,Footer,Possibility, WhatGPT3 } from './container'
-import { Brand, CTA, Feature , Navbar} from './components'
-import './App.css'
+import './App.css';
 
-const App = () => {
-  return (
-    <div className='App'>
-      <div className='gradient__bg'>
-        <Navbar />
-        <Header />
-      </div>
-      <Brand />
-      <WhatGPT3 />
-      <Feature />
-      <Possibility />
-      <CTA />
-      <Blog />
-      <Footer />
+const App = () => (
+  <div className="App">
+    <div className="gradient__bg">
+      <Navbar />
+      <Header />
     </div>
-  )
-}
+    <Brand />
+    <WhatGPT3 />
+    <Features />
+    <Possibility />
+    <CTA />
+    <Blog />
+    <Footer />
+  </div>
+);
 
 export default App
