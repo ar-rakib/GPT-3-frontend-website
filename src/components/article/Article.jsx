2a8980600b893a1539a9c13f2d047318c5acb049
ar-rakib <arrakib.github.com>
2023-05-30 12:59:43 +0600

    Article updated

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import './article.css'
 
-const Article = ({imgUrl}) => {
+const Article = ({ imgUrl, date, title }) => {
   return (
     <div className='gp3__blog-container_article'>
       <div className='gpt3__blog-container_article-img'>
         <img src={imgUrl} alt="blog" />
+      </div>
+      <div className='gpt__blog-container_article-content'>
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        <p>Read Full Article Here</p>
       </div>
     </div>
   )
